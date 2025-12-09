a1e1ff5ae1420217dcba9d6c29c5c5d09f154245
teleskop150750 <teleskop150750@gmail.com>
2025-12-10 02:53:54 +0300

    wip ui

--- src/main/java/com/example/SecondaryController.java
+++ src/main/java/com/example/SecondaryController.java
@@ -2,11 +2,17 @@ package com.example;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.application.Platform;
 
 public class SecondaryController {
 
     @FXML
     private void switchToPrimary() throws IOException {
         App.setRoot("primary");
+    }
+
+    @FXML
+    private void onExit() {
+        Platform.exit();
     }
 }
